f1124f3dae30f6e4ec8df8de5ded506d0abdc781
kachamozo <cachamocito@yahoo.com>
2021-12-02 20:22:00 -0400

    created component cardTemp

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,17 @@
 import React from "react";
+import CardTemp from "./CardTemp";
 
 export default function Card(props) {
   // acá va tu código
-  function handleOnclose(){
-    if(typeof props.onClose === "function") props.onClose()
+  function handleOnclose() {
+    if (typeof props.onClose === "function") props.onClose();
   }
   return (
     <>
       <button onClick={handleOnclose}>X</button>
       <h2>{props.name}</h2>
-      <div>
-        <label>Min</label>
-        <span>{props.min}</span>
-      </div>
-      <div>
-        <label>Max</label>
-        <span>{props.max}</span>
-      </div>
+      <CardTemp label="Min" value={props.min} />
+      <CardTemp label="Max" value={props.max} />
       <img
         src={`http://openweathermap.org/img/wn/${props.img}@2x.png`}
         alt="imagen de clima"
@@ -24,3 +19,5 @@ export default function Card(props) {
     </>
   );
 }
+
+
